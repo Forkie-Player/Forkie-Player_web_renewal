a7b5a9d0b7fe745818038441f366f21ba35f9227
Seongil-Shin <callmeshin75@gmail.com>
2022-01-28 18:11:40 +0900

    fix : #6

--- src/components/elements/CustomButton.tsx
+++ src/components/elements/CustomButton.tsx
@@ -33,13 +33,13 @@ const CustomButton = forwardRef<HTMLDivElement, ITextButtonProps>(
       <div
         ref={ref}
         {...props}
-        className={clsx(
+        className={clsx([
           getElementSizeWrapper(size),
           'p-1 shadow-outer text-center hover:drop-shadow-md active:shadow-inner cursor-pointer',
           type === 'primary' && 'bg-redrose-light text-white',
           type === 'secondary' && 'bg-white text-redrose-light',
           props.className,
-        )}
+        ])}
       >
         {text}
       </div>

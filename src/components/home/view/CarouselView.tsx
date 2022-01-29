eb58d47d5bb5b3613da7c81326caaa680340fe62
Seongil-Shin <callmeshin75@gmail.com>
2022-01-29 22:25:57 +0900

    style : landing image

--- src/components/home/view/CarouselView.tsx
+++ src/components/home/view/CarouselView.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Keyboard, Autoplay } from 'swiper'
+import landingFirst from '../../../assets/images/landing_first.png'
 
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/keyboard'
 import 'swiper/css/autoplay'
 
-const imageList = [
-  'https://www.liveabout.com/thmb/pwO4o_iDrZRTmmhs7eOfD25Qoqw=/1500x1125/smart/filters:no_upscale()/pop-music-57bce3863df78c87634ea806.jpg',
-  'https://freemusicdownloads.world/wp-content/uploads/2020/03/pop-music-1.jpg',
-  'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fd12b3eb0-10ec-11e8-aa39-e7299ff3a5e8.jpg?crop=3000%2C1687%2C0%2C156&resize=1200',
-  'https://wallpaperaccess.com/full/2742175.jpg',
-]
+const imageList = [landingFirst]
 
 function CarouselView() {
   return (

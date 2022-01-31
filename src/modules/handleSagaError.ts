c33b2dbbd777f98a7d3dca0acf81ca6dda69ff13
Seongil-Shin <callmeshin75@gmail.com>
2022-01-31 20:11:10 +0900

    feat : connect sentty, analytics/ make sitemap

--- src/modules/handleSagaError.ts
+++ src/modules/handleSagaError.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 import { put } from 'redux-saga/effects'
 import { PayloadAction } from 'typesafe-actions'
-
+import * as Sentry from '@sentry/react'
 import * as Strings from '../lib/strings'
 
 export default function* handleSagaError(err: unknown, failure: (payload: string) => PayloadAction<string, string>) {
@@ -14,6 +14,7 @@ export default function* handleSagaError(err: unknown, failure: (payload: string
         break
     }
   }
+  yield Sentry.captureException(`Catched Error : ${err}`)
   yield put(failure(errorMessage))
   toast.error(errorMessage)
 }
